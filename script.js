fbb552bdc0bb27d2d2e1dfe051134faa7646b26f
rajeshmondal54 <rm4223536@gmail.com>
2024-07-25 00:54:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	  event.preventDefault();
+  const firstName = document.getElementById('fname').value;
+  const lastName = document.getElementById('lname').value;
+  alert(`${firstName} ${lastName}`);
 
 }
